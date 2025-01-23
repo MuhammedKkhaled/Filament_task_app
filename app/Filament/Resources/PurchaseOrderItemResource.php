d52046a7dd06da08761da59f142646661f07c047
MuhammedKhaled <mohamed.khaled@areebgroup.com>
2025-01-23 16:23:21 +0200

    Enhance PurchaseOrderItemResource with additional fields and improved relationships

--- app/Filament/Resources/PurchaseOrderItemResource.php
+++ app/Filament/Resources/PurchaseOrderItemResource.php
@@ -4,12 +4,17 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PurchaseOrderItemResource\Pages;
 use App\Filament\Resources\PurchaseOrderItemResource\RelationManagers;
+use App\Models\PurchaseOrder;
 use App\Models\PurchaseOrderItem;
 use Filament\Forms;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -25,13 +30,30 @@ class PurchaseOrderItemResource extends Resource
         return $form
             ->schema([
                 Select::make('purchase_order_id')
+                    ->label('Purchase Order')
                     ->options(
-                        PurchaseOrderResource::all()
-                            ->map(fn($record) => "{$record->id} - Total-Cost", 'id')
-                            ->toArray(),
+                        PurchaseOrder::query()->get()
+                            ->mapWithKeys(fn($record) => [$record->id => "{$record->id} - {$record->total_cost} - Supplier ( {$record->supplier->name}  )"])
                     )
-                    ->displayUsingLabels()
+                    ->searchable()
+                    ->preload()
                     ->required(),
+                Select::make('product_id')
+                    ->relationship('product', 'name')
+                    ->preload()
+                    ->searchable()
+                    ->required(),
+                TextInput::make('quantity')
+                    ->numeric()
+                    ->minValue(1)
+                    ->rules(['required', 'min:1', 'max:200'])
+                    ->required(),
+                TextInput::make('price')
+                    ->numeric()
+                    ->minValue(0.01)
+                    ->rules(['required', 'min:0.01', 'max:999999.99'])
+                    ->required(),
+
             ]);
     }
 
@@ -39,13 +61,26 @@ class PurchaseOrderItemResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('purchaseOrder.total_cost')
+                    ->label('Purchase Order'),
+                TextColumn::make('product.name')
+                    ->label('Product')
+                    ->searchable(),
+                TextColumn::make('quantity')
+                    ->label('Quantity'),
+                TextColumn::make('price')
+                    ->label('Price'),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+
+                ActionGroup::make([
+                    DeleteAction::make(),
+                    Tables\Actions\EditAction::make(),
+
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
